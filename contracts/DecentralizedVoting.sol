11305ec7fb23920e5c12920e381f3583fb7f11c2
PartthK <109812828+PartthK@users.noreply.github.com>
2024-04-15 19:31:10 -0400

    Update DecentralizedVoting.sol

--- contracts/DecentralizedVoting.sol
+++ contracts/DecentralizedVoting.sol
@@ -12,6 +12,9 @@ contract DecentralizedVoting {
     
     // Mapping of proposal ID to Proposal struct
     mapping(uint256 => Proposal) public proposals;
+
+    // Mapping of voter addresses to their weights
+    mapping(address => uint256) public voterWeights;
     
     // Events to emit on proposal creation and voting
     event ProposalCreated(uint256 proposalId, string description);
@@ -31,12 +34,13 @@ contract DecentralizedVoting {
     // Function to vote for or against a proposal
     function vote(uint256 proposalId, bool inSupport) public {
         require(proposals[proposalId].exists, "Proposal does not exist");
+        require(voterWeights[msg.sender] > 0, "You are not authorized to vote.");
         
         Proposal storage proposal = proposals[proposalId];
         if (inSupport) {
-            proposal.forVotes++;
+            proposal.forVotes += voterWeights[msg.sender];
         } else {
-            proposal.againstVotes++;
+            proposal.againstVotes += voterWeights[msg.sender];
         }
         
         emit Voted(proposalId, msg.sender, inSupport);
@@ -48,5 +52,15 @@ contract DecentralizedVoting {
 
         Proposal storage proposal = proposals[proposalId];
         emit VotesCounted(proposalId, proposal.forVotes, proposal.againstVotes);
+    }
+    
+    // Function to assign voting weights to voters
+    function assignVotingWeight(address[] memory voters, uint256[] memory weights) public {
+        require(voters.length == weights.length, "Length mismatch");
+        
+        for (uint256 i = 0; i < voters.length; i++) {
+            require(voterWeights[voters[i]] == 0, "Voter weight already assigned");
+            voterWeights[voters[i]] = weights[i];
+        }
     }
 }
