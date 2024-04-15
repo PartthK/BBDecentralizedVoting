7dd9a6e335136df477ec1ca15a0d940e4982d473
PartthK <109812828+PartthK@users.noreply.github.com>
2024-04-15 19:02:43 -0400

    Update DecentralizedVoting.sol

--- contracts/DecentralizedVoting.sol
+++ contracts/DecentralizedVoting.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.0;
 
 contract DecentralizedVoting {
-    // Structure to represent a proposal
+    struct Voter {
+        uint256 balance;
+        uint256 power; // voting power
+        address delegate; // address to whom they delegate their vote
+    }
+    
     struct Proposal {
         string description;
         uint256 forVotes;
@@ -10,43 +15,47 @@ contract DecentralizedVoting {
         bool exists;
     }
     
-    // Mapping of proposal ID to Proposal struct
+    mapping(address => Voter) public voters;
     mapping(uint256 => Proposal) public proposals;
-    
-    // Events to emit on proposal creation and voting
-    event ProposalCreated(uint256 proposalId, string description);
-    event Voted(uint256 proposalId, address voter, bool inSupport);
-    event VotesCounted(uint256 proposalId, uint256 forVotes, uint256 againstVotes);
 
-    // Function to create a new proposal
+    uint256 public totalVotes;
+    
+    event ProposalCreated(uint256 indexed proposalId, string description);
+    event Voted(uint256 indexed proposalId, address voter, bool inSupport);
+
     function createProposal(uint256 proposalId, string memory description) public {
         require(!proposals[proposalId].exists, "Proposal already exists");
         
-        Proposal memory newProposal = Proposal(description, 0, 0, true);
-        proposals[proposalId] = newProposal;
+        proposals[proposalId] = Proposal(description, 0, 0, true);
         
         emit ProposalCreated(proposalId, description);
     }
     
-    // Function to vote for or against a proposal
     function vote(uint256 proposalId, bool inSupport) public {
         require(proposals[proposalId].exists, "Proposal does not exist");
-        
+        require(voters[msg.sender].balance > 0, "Sender has no voting power");
+
         Proposal storage proposal = proposals[proposalId];
         if (inSupport) {
-            proposal.forVotes++;
+            proposal.forVotes += voters[msg.sender].power;
         } else {
-            proposal.againstVotes++;
+            proposal.againstVotes += voters[msg.sender].power;
         }
+
+        voters[msg.sender].balance -= voters[msg.sender].power; // remove the power from balance
         
         emit Voted(proposalId, msg.sender, inSupport);
     }
 
-    // Function to count votes for a proposal
-    function countVotes(uint256 proposalId) public {
-        require(proposals[proposalId].exists, "Proposal does not exist");
-
-        Proposal storage proposal = proposals[proposalId];
-        emit VotesCounted(proposalId, proposal.forVotes, proposal.againstVotes);
+    function delegate(address to) public {
+        require(to != msg.sender, "Self-delegation is not allowed");
+        
+        voters[msg.sender].delegate = to;
+        uint256 power = voters[msg.sender].power;
+        voters[to].power += power; // add the delegated power to delegate's power
+        voters[msg.sender].power = 0; // reset sender's power
+        
+        // Update total votes
+        totalVotes += power;
     }
 }
