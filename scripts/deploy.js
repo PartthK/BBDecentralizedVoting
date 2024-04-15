279f388c9821147451eb49536af6982ab1600c91
PartthK <109812828+PartthK@users.noreply.github.com>
2024-04-15 19:40:07 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,16 +3,8 @@ const { ethers } = require("hardhat");
 async function main() {
   const DecentralizedVoting = await ethers.getContractFactory("DecentralizedVoting");
   const decentralizedVoting = await DecentralizedVoting.deploy();
-  
-  await decentralizedVoting.deployed();
 
-  // Test the new functionality: assign voting weights
-  const [owner, addr1, addr2] = await ethers.getSigners();
-  await decentralizedVoting.assignVotingWeight([addr1.address, addr2.address], [100, 200]);
-  const weight1 = await decentralizedVoting.voterWeights(addr1.address);
-  const weight2 = await decentralizedVoting.voterWeights(addr2.address);
-  console.log("Voting weight for address 1:", weight1.toString());
-  console.log("Voting weight for address 2:", weight2.toString());
+  await decentralizedVoting.deployed();
 
   console.log("DecentralizedVoting contract deployed to:", decentralizedVoting.address);
 }
