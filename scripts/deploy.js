d74bd73301f47d313f24fe81b4b0f4837b1997e3
PartthK <109812828+PartthK@users.noreply.github.com>
2024-04-15 19:21:57 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,15 +3,12 @@ const { ethers } = require("hardhat");
 async function main() {
   const DecentralizedVoting = await ethers.getContractFactory("DecentralizedVoting");
   const decentralizedVoting = await DecentralizedVoting.deploy();
-  
   await decentralizedVoting.deployed();
-  
+
   console.log("DecentralizedVoting contract deployed to:", decentralizedVoting.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
