28ecd77fb11c4e8747990019b910b1ec90ad455d
PartthK <109812828+PartthK@users.noreply.github.com>
2024-04-15 18:53:22 -0400

    Update DecentralizedVoting.test.js

--- test/DecentralizedVoting.test.js
+++ test/DecentralizedVoting.test.js
@@ -4,6 +4,8 @@ const { expect } = require("chai");
 describe("DecentralizedVoting", function () {
   let DecentralizedVoting;
   let decentralizedVoting;
+  let Lock;
+  let lock;
   let owner;
   let addr1;
   let addr2;
@@ -11,10 +13,15 @@ describe("DecentralizedVoting", function () {
 
   async function deployContract() {
     [owner, addr1, addr2] = await ethers.getSigners();
+
     DecentralizedVoting = await ethers.getContractFactory("DecentralizedVoting");
     decentralizedVoting = await DecentralizedVoting.deploy();
+    await decentralizedVoting.deployed();
+
+    Lock = await ethers.getContractFactory("Lock");
+    lock = await Lock.deploy(Math.round(Date.now() / 1000) + 60, { value: ethers.utils.parseEther("0.001") });
+    await lock.deployed();
   }
-  
 
   describe("Create a proposal", function () {
     it("Should create a new proposal (+ 12 points)", async function () {
@@ -55,9 +62,26 @@ describe("DecentralizedVoting", function () {
     });
   });
 
+  describe("Count votes for a proposal", function () {
+    it("Should count the votes for a proposal (+ 10 points)", async function () {
+      await deployContract();
+
+      await decentralizedVoting.connect(addr1).createProposal(1, "Test Proposal");
+      await decentralizedVoting.connect(addr2).vote(1, true);
+      await decentralizedVoting.countVotes(1);
+
+      const proposal = await decentralizedVoting.proposals(1);
+
+      expect(proposal.forVotes).to.equal(1);
+      expect(proposal.againstVotes).to.equal(0);
+
+      points += 10;
+    });
+  });
+
   describe("Total points", function () {
-    it("You should have 32.5/32.5 on this assignment.", async function () {
-      expect(points).to.equal(32.5);
+    it("You should have 42.5/42.5 on this assignment.", async function () {
+      expect(points).to.equal(42.5);
     });
   });
 });
